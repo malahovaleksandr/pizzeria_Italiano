b482d3272bb12c884794063fcc80635568b4d214
“malahovaleksandr” <pro100boy@gmail.com>
2016-09-12 18:17:43 +0300

    корректировка адаптив для телефона и планшета сделан

--- resourse/js/hide_menu_item.js
+++ resourse/js/hide_menu_item.js
@@ -2,26 +2,27 @@ $(document).ready(function(){
     console.log('подключен hide_menu ');
 });
 var humburger=$('.header__nav_svg-burger'),
-    hide_menu=$('.wrapper__header__nav'),
+    close_hide_Menu=$('.header__nav_svg-burger_close'),
+    hide_menu=$('.menu-tel'),
     testHide=1;//показатель когда показывать и прятать меню
 
-humburger.on('click',function(){
-    if(controlAnimate==1){
+humburger.on('click',function(){//показать меню
+           if(testHide==1){
+               hide_menu.fadeIn(10);
+               hide_menu.animate({
+                   'right': '0'
+               });
+               testHide=0;
+           }
 
-        if(testHide>0)
-        {
-            hide_menu.fadeIn(10);
-            hide_menu.animate({
-                'right':'0'
-            });
-            testHide=0;
-        }
-        else{
-            hide_menu.animate({
-                'right':'-100%'
-            });
-            hide_menu.fadeOut();
-            testHide=1;
-        }
+});
+close_hide_Menu.on('click',function(){//спрятать меню
+    if(testHide==0){
+        hide_menu.animate({
+            'right':'-100%'
+        });
+        hide_menu.fadeOut();
+        testHide=1;
     }
+
 });
